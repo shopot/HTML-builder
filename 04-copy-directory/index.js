c21dc01af88a97e2de83ba9bfeda8b5f766c452b
Sapronov Konstantin <ks.costas@gmail.com>
2022-10-28 11:53:28 +0300

    feat: add copy children directories

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,34 +1,44 @@
-const fs = require('fs');
-const path = require('path');
+const nodeFS = require('fs');
+const nodePath = require('path');
 
 /**
  * Copy files from source to destination
  *
  * @param {string} srcDir
  * @param {string} destDir
+ * @returns {void}
  */
-const copyDir = (srcDir, destDir) => {
-  fs.stat(destDir, (err, stats) => {
-    if (err) {
-      fs.mkdir(destDir, { recursive: true }, err => {
-        if (err) throw err;
-      });
-    }
-  });
+const copyDir = async (srcDir, destDir) => {
+  // Create directory if it doesn't exist'
+  await nodeFS.promises.mkdir(destDir, { recursive: true });
 
-  fs.readdir(srcDir, {}, (err, files) => {
-    files.forEach(file => {
-      const filename = path.resolve(srcDir, file);
-      const filenameNew = path.resolve(destDir, file);
-      fs.copyFile(filename, filenameNew, err => {
-        if (err) throw err;
-        console.log(`${file} was copied to destination directory`);
-      });
-    });
-  });
+  // Gte files and directories
+  const files = await nodeFS.promises.readdir(srcDir, { withFileTypes: true });
+
+  for (let i = 0; i < files.length; i++) {
+    const src = nodePath.resolve(srcDir, files[i].name);
+    const dest = nodePath.resolve(destDir, files[i].name);
+
+    if (files[i].isDirectory()) {
+      await copyDir(src, dest);
+    } else {
+      await nodeFS.promises.copyFile(src, dest);
+    }
+  }
 };
 
-const srcDir = path.resolve(__dirname, 'files');
-const destDir = path.resolve(__dirname, 'files-copy');
+/**
+ * Main application function
+ *
+ * @returns {void}
+ */
+const main = async () => {
+  const srcDir = nodePath.resolve(__dirname, 'files');
+  const destDir = nodePath.join(__dirname, 'files-copy');
 
-copyDir(srcDir, destDir);
+  await copyDir(srcDir, destDir);
+
+  console.log('Job was completed successfully.');
+};
+
+main();
